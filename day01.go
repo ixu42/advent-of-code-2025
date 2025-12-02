af97941c36a66996ae67d60cf316d6e16a8519ac
Ivy Xu <xyan21418@gmail.com>
2025-12-02 22:57:44 +0200

    refactoring

--- day01.go
+++ day01.go
@@ -7,6 +7,79 @@ import (
 	"strconv"
 )
 
+func parseRotation(line string) (direction byte, distance int, err error) {
+	direction = line[0]
+	distance, err = strconv.Atoi(line[1:])
+	return direction, distance, err
+}
+
+func rotateAndCount(pos int, direction byte, distance int) (int, int) {
+	countAtZero := 0
+	for i := 0; i < distance; i++ {
+		if direction == 'L' {
+			pos--
+			if pos < 0 {
+				pos = 99
+			}
+		} else if direction == 'R' {
+			pos++
+			if pos > 99 {
+				pos = 0
+			}
+		}
+		if pos == 0 {
+			countAtZero++
+		}
+	}
+	return pos, countAtZero
+}
+
+func solvePart1(lines []string) int {
+	pos := 50
+	countAtZero := 0
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		direction, distance, err := parseRotation(line)
+		if err != nil {
+			fmt.Println("Error parsing rotation:", err)
+			return 0
+		}
+
+		pos, _ = rotateAndCount(pos, direction, distance)
+
+		if pos == 0 {
+			countAtZero++
+		}
+	}
+	return countAtZero
+}
+
+func solvePart2(lines []string) int {
+	pos := 50
+	countAtZero := 0
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		direction, distance, err := parseRotation(line)
+		if err != nil {
+			fmt.Println("Error parsing rotation:", err)
+			return 0
+		}
+
+		tempCount := 0
+		pos, tempCount = rotateAndCount(pos, direction, distance)
+		countAtZero += tempCount
+	}
+	return countAtZero
+}
+
 func main() {
     data, err := os.ReadFile("inputs/day01.txt")
     if err != nil {
@@ -16,73 +89,6 @@ func main() {
 
 	lines := strings.Split(string(data), "\n")
 
-	// part1
-	// pos := 50
-	// countAtZero := 0
-
-	// for _, line := range lines {
-	// 	if line == "" {
-	// 		continue
-	// 	}
-
-	// 	direction := line[0]
-	// 	distance, err := strconv.Atoi(line[1:])
-	// 	if err != nil {
-	// 		fmt.Println("Error parsing distance:", err)
-	// 		return
-	// 	}
-
-	// 	if direction == 'L' {
-    //         pos -= distance
-    //     } else if direction == 'R' {
-    //         pos += distance
-    //     }
-
-	// 	// ensure pos stays within the range 0-99
-	// 	pos = (pos + 100) % 100
-
-	// 	if pos == 0 {
-	// 		countAtZero++
-	// 	}
-	// }
-
-	// fmt.Println("part1:", countAtZero)
-
-	// part2
-	pos := 50
-	countAtZero := 0
-
-	for _, line := range lines {
-		if line == "" {
-			continue
-		}
-
-		direction := line[0]
-		distance, err := strconv.Atoi(line[1:])
-		if err != nil {
-			fmt.Println("Error parsing distance:", err)
-			return
-		}
-
-		if direction == 'L' {
-            for i := 0; i < distance; i++ {
-                pos--
-                if pos < 0 {
-                    pos = 99
-                }
-                if pos == 0 {
-                    countAtZero++
-                }
-            }
-        } else if direction == 'R' {
-            for i := 0; i < distance; i++ {
-                pos++
-                if pos > 99 {
-                    pos = 0
-					countAtZero++
-                }
-            }
-        }
-	}
-	fmt.Println("part2:", countAtZero)
+	fmt.Println("part1:", solvePart1(lines))
+	fmt.Println("part2:", solvePart2(lines))
 }
