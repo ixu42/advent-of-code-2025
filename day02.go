b2791269e1421ecf5f1aa6ede753bc4a90a6062e
Ivy Xu <xyan21418@gmail.com>
2025-12-03 21:34:38 +0200

    day2, part2

--- day02.go
+++ day02.go
@@ -18,6 +18,33 @@ func isRepeatedTwice(n int) bool {
 	return str[:mid] == str[mid:]
 }
 
+func isRepeatedAtLeastTwice(n int) bool {
+	str := strconv.Itoa(n)
+	maxRepeat := len(str)
+
+	for i := 2; i <= maxRepeat; i++ {
+		if len(str) % i != 0 {
+			continue
+		}
+
+		chunkSize := len(str) / i
+		pattern := str[:chunkSize]
+		matched := true
+
+		for j := 1; j < i; j++ {
+			if str[j*chunkSize:(j+1)*chunkSize] != pattern {
+				matched = false
+				break
+			}
+		}
+
+		if matched {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
     data, err := os.ReadFile("inputs/day02.txt")
     if err != nil {
@@ -27,8 +54,34 @@ func main() {
 
 	pairs := strings.Split(string(data), ",")
 
-	res := 0
+	// part1
+	// res := 0
 
+	// for _, pair := range pairs {
+	// 	idPair := strings.Split(pair, "-")
+	// 	if len(idPair) != 2 {
+	// 		fmt.Println("Invalid id pair:", pair)
+	// 		return
+	// 	}
+
+	// 	firstId, err1 := strconv.Atoi(idPair[0])
+	// 	lastId, err2 := strconv.Atoi(idPair[1])
+	// 	if err1 != nil || err2 != nil {
+	// 		fmt.Println("Error converting ids:", err1, err2)
+	// 		return
+	// 	}
+
+	// 	for i := firstId; i <= lastId; i++ {
+	// 		if isRepeatedTwice(i) {
+	// 			res += i
+	// 		}
+	// 	}
+	// }
+
+	// fmt.Println("part1:", res)
+
+	// part2
+	res := 0
 	for _, pair := range pairs {
 		idPair := strings.Split(pair, "-")
 		if len(idPair) != 2 {
@@ -44,11 +97,10 @@ func main() {
 		}
 
 		for i := firstId; i <= lastId; i++ {
-			if isRepeatedTwice(i) {
+			if isRepeatedAtLeastTwice(i) {
 				res += i
 			}
 		}
 	}
-
-	fmt.Println("part1:", res)
+	fmt.Println("part2:", res)
 }
