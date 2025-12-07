50052b991c898d27f21694c501039f54a183ae9c
Ivy Xu <xyan21418@gmail.com>
2025-12-07 07:43:30 +0200

    refactoring

--- day06.go
+++ day06.go
@@ -7,62 +7,72 @@ import (
 	"strconv"
 )
 
-func main() {
-    data, err := os.ReadFile("inputs/day06.txt")
-    if err != nil {
-        fmt.Println("Error reading file:", err)
-        return
-    }
+func solvePart1(lines []string) int {
+	num0 := []int{}
+	num1 := []int{}
+	num2 := []int{}
+	num3 := []int{}
+	ops := []string{}
+	for i, line := range lines {
+		fields := strings.Fields(line)
+		for _, f := range fields {
+			if i == 4 {
+				ops = append(ops, f)
+				continue
+			}
+			n, err := strconv.Atoi(f)
+			if err != nil {
+				fmt.Println("Error converting to int:", err)
+				return 0
+			}
+			if i == 0 {
+				num0 = append(num0, n)
+				continue
+			}
+			if i == 1 {
+				num1 = append(num1, n)
+				continue
+			}
+			if i == 2 {
+				num2 = append(num2, n)
+				continue
+			}
+			num3 = append(num3, n)	
+		}
+	}
 
-	lines := strings.Split(string(data), "\n")
+	res := 0
+	for i, op := range ops {
+		switch op {
+			case "+":
+				res += num0[i] + num1[i] + num2[i] + num3[i]
+			case "*":
+				res += num0[i] * num1[i] * num2[i] * num3[i]
+		}
+	}
+	return res
+}
 
-	// part1
-	// num0 := []int{}
-	// num1 := []int{}
-	// num2 := []int{}
-	// num3 := []int{}
-	// ops := []string{}
-	// for i, line := range lines {
-	// 	fields := strings.Fields(line)
-	// 	for _, f := range fields {
-	// 		if i == 4 {
-	// 			ops = append(ops, f)
-	// 			continue
-	// 		}
-	// 		n, err := strconv.Atoi(f)
-	// 		if err != nil {
-	// 			fmt.Println("Error converting to int:", err)
-	// 			return
-	// 		}
-	// 		if i == 0 {
-	// 			num0 = append(num0, n)
-	// 			continue
-	// 		}
-	// 		if i == 1 {
-	// 			num1 = append(num1, n)
-	// 			continue
-	// 		}
-	// 		if i == 2 {
-	// 			num2 = append(num2, n)
-	// 			continue
-	// 		}
-	// 		num3 = append(num3, n)	
-	// 	}
-	// }
+func calculate(nums []int, op string) int {
+	res := 0
+	switch op {
+		case "+":
+			sum := 0
+			for _, n := range nums {
+				sum += n
+			}
+			res += sum
+		case "*":
+			prod := 1
+			for _, n := range nums {
+				prod *= n
+			}
+			res += prod
+	}
+	return res
+}
 
-	// res := 0
-	// for i, op := range ops {
-	// 	switch op {
-	// 		case "+":
-	// 			res += num0[i] + num1[i] + num2[i] + num3[i]
-	// 		case "*":
-	// 			res += num0[i] * num1[i] * num2[i] * num3[i]
-	// 	}
-	// }
-
-	// fmt.Println("part1:", res)
-
-	// part2
+func solvePart2(lines []string) int {
 	res := 0
 	grid := make([][]rune, len(lines))
 	for i, line := range lines {
@@ -75,29 +85,13 @@ func main() {
 	op := ' '
 	for i := 0; i < len(grid[0]); i++ {
 		char := grid[0][i]
-		// calculate for prev problem when seeing an operator
-		if grid[4][i] != ' ' {
-			switch op {
-				case '+':
-					sum := 0
-					for _, n := range nums {
-						sum += n
-					}
-					println("sum:", sum)
-					res += sum
-				case '*':
-					prod := 1
-					for _, n := range nums {
-						prod *= n
-					}
-					println("prod:", prod)
-					res += prod
-				default:
-					println("something went wrong")
-			}
+		// calculate for prev problem when seeing a new operator
+		NewOp := grid[4][i]
+		if NewOp != ' ' {
+			res += calculate(nums, string(op))
 			nums = []int{}
 			numStr = ""
-			op = grid[4][i]
+			op = NewOp
 		}
 
 		numStr = string(char) + string(grid[1][i]) + string(grid[2][i]) + string(grid[3][i])
@@ -105,32 +99,26 @@ func main() {
 		if numStr == "" {
 			continue
 		}
-		println("i:", i)
-		println("op:", string(op))
-		println("numStr:", numStr)
 		num, err := strconv.Atoi(numStr)
 		if err != nil {
 			fmt.Println("Error converting to int:", err)
-			return
+			return 0
 		}
-		println("num:", num)
 		nums = append(nums, num)
 	}
-	switch op {
-		case '+':
-			sum := 0
-			for _, n := range nums {
-				sum += n
-			}
-			println("sum:", sum)
-			res += sum
-		case '*':
-			prod := 1
-			for _, n := range nums {
-				prod *= n
-			}
-			println("prod:", prod)
-			res += prod
-	}
-	fmt.Println("part2:", res)
+	res += calculate(nums, string(op))
+	return res
+}
+
+func main() {
+    data, err := os.ReadFile("inputs/day06.txt")
+    if err != nil {
+        fmt.Println("Error reading file:", err)
+        return
+    }
+
+	lines := strings.Split(string(data), "\n")
+
+	fmt.Println("part1:", solvePart1(lines))
+	fmt.Println("part2:", solvePart2(lines))
 }
