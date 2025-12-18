1f56ef73ef697a88cfe79b70fdb4016cd1f9789e
Ivy Xu <xyan21418@gmail.com>
2025-12-18 15:59:38 +0200

    refactoring

--- day08.go
+++ day08.go
@@ -9,6 +9,8 @@ import (
 	"sort"
 )
 
+var boxes []box
+var conns []connection
 var parent []int
 var size []int
 
@@ -23,11 +25,46 @@ type connection struct {
 	dist float64
 }
 
+func getAllBoxPositions(lines []string) error {
+	boxes = make([]box, 0)
+	for _, line := range lines {
+		vals := strings.Split(line, ",")
+		if len(vals) != 3 {
+			return fmt.Errorf("Invalid line format:", line)
+		}
+		xStr, yStr, zStr := vals[0], vals[1], vals[2]
+		x, errX := strconv.ParseFloat(xStr, 64)
+		y, errY := strconv.ParseFloat(yStr, 64)
+		z, errZ := strconv.ParseFloat(zStr, 64)
+		if errX != nil || errY != nil || errZ != nil {
+			return fmt.Errorf("Error converting string to int:", errX, errY, errZ)
+		}
+		boxes = append(boxes, box{x, y, z})
+	}
+
+	return nil
+}
+
 func getDistance(a, b box) float64 {
 	dx := a.x - b.x
 	dy := a.y - b.y
 	dz := a.z - b.z
 	return math.Sqrt(dx*dx + dy*dy + dz*dz)
+}
+
+// get connections between each pair of boxes
+func getAndSortConnnections() {
+	for i := 0; i < len(boxes); i++ {
+		for j := i + 1; j < len(boxes); j++ {
+			dist := getDistance(boxes[i], boxes[j])
+			conns = append(conns, connection{i, j, dist})
+		}
+	}
+
+	// from the closest to the furthest
+	sort.Slice(conns, func(a, b int) bool {
+		return conns[a].dist < conns[b].dist
+	})
 }
 
 // find root / representative of the connected boxes
@@ -56,81 +93,16 @@ func union(x, y int) {
 	size[rootX] += size[rootY]
 }
 
-func ifAllConnected() bool {
-	root := find(0)
-	for i := 1; i < len(parent); i++ {
-		if find(i) != root {
-			return false
-		}
+func solvePart1() int {
+	limit := 1000
+	if limit > len(conns) {
+		limit = len(conns)
 	}
-	return true
-}
-
-func main() {
-    data, err := os.ReadFile("inputs/day08.txt")
-    if err != nil {
-        fmt.Println("Error reading file:", err)
-        return
-    }
-
-	lines := strings.Split(string(data), "\n")
-
-	// get a list of positions of all junction boxes
-	boxes := make([]box, 0)
-	for _, line := range lines {
-		vals := strings.Split(line, ",")
-		if len(vals) != 3 {
-			fmt.Println("Invalid line format:", line)
-			return
-		}
-		xStr, yStr, zStr := vals[0], vals[1], vals[2]
-		x, errX := strconv.ParseFloat(xStr, 64)
-		y, errY := strconv.ParseFloat(yStr, 64)
-		z, errZ := strconv.ParseFloat(zStr, 64)
-		if errX != nil || errY != nil || errZ != nil {
-			fmt.Println("Error converting string to int:", errX, errY, errZ)
-			return
-		}
-		boxes = append(boxes, box{x, y, z})
-	}
-
-	// get connections between each pair of boxes
-	conns := make([]connection, 0)
-	for i := 0; i < len(boxes); i++ {
-		for j := i + 1; j < len(boxes); j++ {
-			dist := getDistance(boxes[i], boxes[j])
-			// fmt.Printf("Distance between pos %d and pos %d: %.2f\n", i, j, dist)
-			conns = append(conns, connection{i, j, dist})
-		}
-	}
-
-	// from the closest to the furthest
-	sort.Slice(conns, func(a, b int) bool {
-		return conns[a].dist < conns[b].dist
-	})
-
-	parent = make([]int, len(boxes))
-	size = make([]int, len(boxes))
-	for i := 0; i < len(boxes); i++ {
-		parent[i] = i // each box starts in its own circuit
-		size[i] = 1 // each circuit starts with size 1
-	}
-
-	// limit := 1000
-	// if limit > len(conns) {
-	// 	limit = len(conns)
-	// }
-	// for k := 0; k < limit; k++ {
-	lastPair := make([]int, 2)
-	for k := 0; k < len(conns); k++ {
+	for k := 0; k < limit; k++ {
 		conn := conns[k]
-		// fmt.Printf("box %d and box %d: distance %.2f\n", conn.a, conn.b, conn.dist)
-		// if not already connected, connect them and if all connected, stop
-		if find(conn.a) != find(conn.b) && !ifAllConnected() {
+		// if not already connected, connect them
+		if find(conn.a) != find(conn.b) {
 			union(conn.a, conn.b)
-			lastPair[0] = conn.a
-			lastPair[1] = conn.b
-			// fmt.Printf("Connected box %d and box %d\n", conn.a, conn.b)
 		}
 	}
 
@@ -140,10 +112,6 @@ func main() {
 		root := find(i)
 		circuitCounts[root]++
 	}
-
-	// for root, count := range circuitCounts {
-	// 	fmt.Printf("circuit root %d has size %d\n", root, count)
-	// }
 
 	// collect and sort sizes
 	sizes := make([]int, 0, len(circuitCounts))
@@ -157,8 +125,58 @@ func main() {
 		res *= sizes[i]
 	}
 
-	// fmt.Printf("part1: %d\n", res)
+	return res
+}
 
-	fmt.Printf("part2: %d\n", int(boxes[lastPair[0]].x*boxes[lastPair[1]].x))
+func ifAllConnected() bool {
+	root := find(0)
+	for i := 1; i < len(parent); i++ {
+		if find(i) != root {
+			return false
+		}
+	}
+	return true
+}
 
+func solvePart2() int {
+	lastPair := make([]int, 2)
+	for k := 0; k < len(conns); k++ {
+		conn := conns[k]
+		// if not already connected, connect them, and if all connected, stop
+		if find(conn.a) != find(conn.b) && !ifAllConnected() {
+			union(conn.a, conn.b)
+			lastPair[0] = conn.a
+			lastPair[1] = conn.b
+		}
+	}
+
+	return int(boxes[lastPair[0]].x*boxes[lastPair[1]].x)
+}
+
+func main() {
+    data, err := os.ReadFile("inputs/day08.txt")
+    if err != nil {
+        fmt.Println("Error reading file:", err)
+        return
+    }
+
+	lines := strings.Split(string(data), "\n")
+
+	if err := getAllBoxPositions(lines); err != nil {
+		fmt.Println("Error getting box positions:", err)
+		return
+	}
+
+	conns = make([]connection, 0)
+	getAndSortConnnections()
+
+	parent = make([]int, len(boxes))
+	size = make([]int, len(boxes))
+	for i := 0; i < len(boxes); i++ {
+		parent[i] = i // each box starts in its own circuit/tree
+		size[i] = 1 // each circuit/tree starts with size 1
+	}
+
+	fmt.Printf("part1: %d\n", solvePart1())
+	fmt.Printf("part2: %d\n", solvePart2())
 }
